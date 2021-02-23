e51be0d33783c1e5207c5438bf0aa9533375aaf3
javierclavijo <javierclavijog@gmail.com>
2021-02-23 18:43:01 +0100

    Add totalLikes, favoriteBlog functions and tests

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => blogs.map(b => b.likes).reduce((a, c) => a + c, 0)
+
+const favoriteBlog = (blogs) => {
+    let blog = blogs.find(b => b.likes === Math.max(...blogs.map(b => b.likes)));
+    return {title: blog.title, author: blog.author, likes: blog.likes};
+}
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }

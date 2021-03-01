07a3f6d0acd64fc4e23ac8f73fc33c064a7262f2
javierclavijo <javierclavijog@gmail.com>
2021-03-01 13:35:56 +0100

    Add functionality to enable DELETE, PUT requests (exercises 4.13, 4.14)

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -21,6 +21,8 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+
 
 module.exports = app
--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -16,5 +16,28 @@ blogsRouter.post('/', async (request, response) => {
     }
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body;
+    const newBlog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    Blog.findByIdAndUpdate(request.params.id, newBlog, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+
+    // const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, {new: true})
+    // response.json(updatedBlog)
+})
 
 module.exports = blogsRouter

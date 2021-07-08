5c29c603241c2dc284a766c4f083550c70ecc26c
javier <javierclavijog@gmail.com>
2021-07-08 14:16:06 +0200

    (5.17) Add support for Cypress testing

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const cors = require('cors')
+const cors = require("cors")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
-const loginRouter = require('./controllers/login')
-const mongoose = require('mongoose')
-const middleware = require('./utils/middleware')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const loginRouter = require("./controllers/login")
+const mongoose = require("mongoose")
+const middleware = require("./utils/middleware")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
 
 mongoose.connect(config.MONGODB_URI, {
     useNewUrlParser: true,
@@ -16,10 +16,10 @@ mongoose.connect(config.MONGODB_URI, {
     useCreateIndex: true
 })
     .then(() => {
-        logger.info('connected to MongoDB')
+        logger.info("connected to MongoDB")
     })
     .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
+        logger.error("error connecting to MongoDB:", error.message)
     })
 
 app.use(cors())
@@ -29,10 +29,14 @@ app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 app.use(middleware.userExtractor)
 
-app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 
+if (process.env.NODE_ENV === "test") {
+    const testingRouter = require("./controllers/testing")
+    app.use("/api/testing", testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
